b20a0db06fd307f0907e0031fb93244c22cbf89a
bush688 <32064695+bush688@users.noreply.github.com>
2020-10-02 21:56:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-# git clone https://github.com/xiaorouji/openwrt-package.git package/lienol
+# git clone https://github.com/fw876/helloworld
